241021ac1b333adadc31c4a75612e910a90940aa
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-07 18:44:08 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,10 +14,10 @@ contacts = {
   }
 }
   
-contacts.each do |person, contact_details_hash|
-  binding.pry
-end
+
   
 def remove_strawberry(contacts)
-
+ contacts.each do |person, contact_details_hash|
+  binding.pry
+ end
 end
