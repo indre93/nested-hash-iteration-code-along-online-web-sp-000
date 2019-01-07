6fd33fa62b8d68bb8b8ca52e41265ce166dd4a0d
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-07 18:49:00 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,9 @@ contacts = {
 def remove_strawberry(contacts)
  contacts.each do |person, contact_details_hash|
   contact_details_hash.each do |attribute, data|
-    binding.pry
-   end
- end
+    if attribute == :favorite_ice_cream_flavors
+      binding.pry
+    end
+  end
+end
 end
